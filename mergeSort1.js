1101d77e3abae7e39e9593a79d440422781ef4c2
dkang417 <dkang417@gmail.com>
2018-11-10 09:35:18 -0500

    add mergesort1

---
+++ mergeSort1.js
@@ -0,0 +1,16 @@
+function mergeSort(arr) {
+    if (arr.length === 1) {
+        return arr;
+    }
+    const middle = Math.floor(arr.length / 2);
+    const left = arr.slice(0, middle);
+    const right = arr.slice(middle);
+
+    return merge(
+        mergeSort(left),
+        mergeSort(right)
+    )
+}
+function merge(left, right) {
+    
+}
