6f911ac9933dd5815edd60e01a73b6bd9ea0113c
dkang417 <dkang417@gmail.com>
2018-11-12 20:23:45 -0500

    update sort

--- mergeSort1.js
+++ mergeSort1.js
@@ -21,19 +21,36 @@
 
 
 
-function mergeSort(arr) {
-    if (arr.length === 1) {
-        return arr;
-    }
-    const middle = Math.floor(arr.length / 2);
-    const left = arr.slice(0, middle);
-    const right = arr.slice(middle);
+function mergeSortRecursive(arr) {
+    // base case
+    if (arr.length <= 1) return arr;
+    
+    // divide and conquer 
+    const leftHalf = array.slice(0, array.length / 2);
+    const rightHalf = array.slice(array.length / 2);
+    const leftSorted = mergeSortRecursive(leftHalf);
+    const rightSorted = mergeSortRecursive(rightHalf);
+ 
 
-    return merge(
-        mergeSort(left),
-        mergeSort(right)
-    )
-}
+    return merge(leftSorted, rightSorted);
+};
+
 function merge(left, right) {
+    var result = [], iLeft = 0, iRight = 0;
+  
+    // while result is not fully populated
+    while (result.length < (left.length + right.length)) {
+      // if all elements in left have been added, then add remaining right elements
+      if (iLeft === left.length) result = result.concat(right.slice(iRight));
+      // if all elements in right have been added, then add remaining left elements
+      else if (iRight === right.length) result = result.concat(left.slice(iLeft));
+      // compare elements in subarrays and add lower of the two to result
+      else if (left[iLeft] <= right[iRight]) result.push(left[iLeft++]);
+      else result.push(right[iRight++]);
+    }
+    return result;
+}
+  
+function mergeSortIterative(array) {
     
 }
